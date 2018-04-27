60c73e296863c4682e19fb8119f6a7250128e1da
Anderson <anderson@sinprosp.org.br>
2018-04-27 16:14:12 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,42 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { CameraPage } from '../pages/camera/camera';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {CameraListPage} from "../pages/camera-list/camera-list";
+import {Camera} from "@ionic-native/camera";
+import {FilePath} from "@ionic-native/file-path";
+import {SQLite} from "@ionic-native/sqlite";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        ListPage,
+        CameraPage,
+        CameraListPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        ListPage,
+        CameraPage,
+        CameraListPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        Camera,
+        FilePath,
+        SQLite,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
 export class AppModule {}
